a26b66589b8ecd0e82df944a42f6bf3d11b8641f
miguel <m.gutierrez@peak-14.de>
2021-12-01 11:35:06 +0100

    set configuration with annotation bean: -setup EnglishService - setupSpanishService

--- src/main/java/com/example/exampleprojectdi/services/config/GreetingServiceConfig.java
+++ src/main/java/com/example/exampleprojectdi/services/config/GreetingServiceConfig.java
@@ -1,24 +1,16 @@
 package com.example.exampleprojectdi.services.config;
 
-import com.example.exampleprojectdi.services.ConstructorGreetingService;
-import com.example.exampleprojectdi.services.PropertyGreetingService;
-import com.example.exampleprojectdi.services.SetterGreetingService;
+import com.example.exampleprojectdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Configuration
 public class GreetingServiceConfig {
-
-    static ConstructorGreetingService constructorGreetingService;
-    static PropertyGreetingService propertyGreetingService;
-    static SetterGreetingService setterGreetingService;
-
-    @Bean
-    public static ConstructorGreetingService getConstructorGreetingService() {
-        return Objects.requireNonNullElseGet(constructorGreetingService, ConstructorGreetingService::new);
-    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
@@ -29,9 +21,38 @@ public class GreetingServiceConfig {
     PropertyGreetingService propertyGreetingService() {
         return new PropertyGreetingService();
     }
+
     @Bean
     SetterGreetingService setterGreetingService() {
         return new SetterGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreeting() {
+        return new PrimaryGreetingService();
+    }
+
+    @Bean("i18nService")
+    @Profile({"EN","default"})
+    I18nEnglishService i18nEnglishService() {
+        return new I18nEnglishService();
+    }
+
+    @Bean("i18nService")
+    @Profile("ES")
+    I18nSpanishService i18nSpanishService() {
+        return new I18nSpanishService();
+    }
+
+    /*
+    static ConstructorGreetingService constructorGreetingService;
+    static PropertyGreetingService propertyGreetingService;
+    static SetterGreetingService setterGreetingService;
+
+    @Bean
+    public static ConstructorGreetingService getConstructorGreetingService() {
+        return Objects.requireNonNullElseGet(constructorGreetingService, ConstructorGreetingService::new);
     }
     @Bean
     public static PropertyGreetingService getPropertyGreetingService() {
@@ -42,5 +63,5 @@ public class GreetingServiceConfig {
     public static SetterGreetingService getSetterGreetingService() {
         return Objects.requireNonNullElseGet(setterGreetingService, SetterGreetingService::new);
     }
-
+ */
 }
